bb1d7b5856a04d77409b80f36a2155906b101a55
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-22 22:29:00 +0530

    update

--- DOM/Background generator/script.js
+++ DOM/Background generator/script.js
@@ -3,10 +3,6 @@ var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
 
-console.log(css);
-console.log(color1);
-console.log(color2);
-
 function col(){
     body.style.background =
         "linear-gradient(to right, " + color1.value
