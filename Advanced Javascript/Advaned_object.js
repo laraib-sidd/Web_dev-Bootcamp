0e6c2b4d2e9a0edfa3ab9968e0272f1595c59e9f
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-23 21:52:24 +0530

    update

--- Advanced Javascript/Advaned_object.js
+++ Advanced Javascript/Advaned_object.js
@@ -1,7 +1,15 @@
 // refrence type
+
 let obj1 = {value: 10};
 let obj2 = obj1;
 let obj3 = {value : 10};
 
-// context
+// context vs scope
+
+this.alert('hello');
+// this refers to what object you are inside of.
+const obj = {
+    a: () => console.log(this),
+}
+
 // instantiation
