14807634aee2ca419a04b721dd8e8fc0828da790
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-23 21:13:00 +0530

    update

--- Advanced Javascript/Advanced_array.js
+++ Advanced Javascript/Advanced_array.js
@@ -2,6 +2,20 @@ const arr = [1,2,3,4];
 const double = [];
 
 const new_array = arr.forEach((num) => {
-    double.push(num*2);
-}
+    doubel.push(num*2);
+})
 
+// map : It always returns an operation.
+const map_arr = arr.map(num => {
+    return num * 5;
+})
+const map_array = arr.map(num => num * 2);
+
+// filter : It does a condition check.
+const filter_arr = arr.filter(num => {
+    return num > 5;
+})
+
+const filter_array = arr.filter(num => num > 5);
+
+// reduce
