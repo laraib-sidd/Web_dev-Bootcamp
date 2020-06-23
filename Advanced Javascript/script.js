946c9b2eebd6e31073423520612b7f4aaf2cc333
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-23 13:16:16 +0530

    let

--- Advanced Javascript/script.js
+++ Advanced Javascript/script.js
@@ -1,3 +1,5 @@
+// Switch
+
 function move(params) {
     var what;
     switch(direction){
@@ -18,3 +20,23 @@ function move(params) {
     }
     return what;
 }
+
+// Terenary operator
+
+// condition ? exp1 : exp2 ;
+
+// if condition true return exp1 else exp2.
+
+// ES 5
+// let , const
+
+const player = "bobby";
+let experience = 100;
+let wizard = false;
+
+if (experience>90){
+    let wizard = true;
+    console.log('inside', wizard);
+}
+
+console.log('outside', wizard);
