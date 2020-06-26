5444466444d433342862c34f3f0536f10ecc87db
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-26 20:35:52 +0530

    update

--- React/Robot_freinds/src/App.js
+++ React/Robot_freinds/src/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import CardList from './CardList'
-import { robots } from './robots';
+// import { robots } from './robots';
 import  SearchBox from './SearchBox'
 import './App.css'
 
@@ -8,9 +8,14 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-        robots : robots,
+        robots : [],
         searchfield : ''
         }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots:users}))
     }
     onSearchChange = (event) =>{
     this.setState({searchfield :event.target.value})
