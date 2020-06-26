c6efea70e5122695ff951d21b4ddf9d67689b04c
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-26 17:20:43 +0530

    update

--- React/anime_list/src/App.js
+++ React/anime_list/src/App.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import CardList from './CardList'
+import { robots } from './robots';
+
+const App = (robots) => {
+    return(
+        <CardList robots = {robots}/>
+    )
+}
+
+export default App;
--- React/anime_list/src/index.js
+++ React/anime_list/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots'
 
 ReactDOM.render(
   <React.StrictMode>
