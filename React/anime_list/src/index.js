10a3a1455d36774b239458612fb1b91006d52f67
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-26 11:41:32 +0530

    new start

--- React/anime_list/src/index.js
+++ React/anime_list/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './Hello'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={"Hello" + "React Ninja"}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
