f14717b332469adfc81a1b9501ab82ba97626d93
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-26 15:41:56 +0530

    update

--- React/anime_list/src/Card.js
+++ React/anime_list/src/Card.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Card = (props) => {
-    return(
+const Card = ({name,email,id}) => {
+    return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src="https://robohash.org/test?200*200" alt="Anime" />
-            <h2>{props.name}</h2>
-            <p>{props.email}</p>
+            <img src={`https://robohash.org/${id}?200*200`} alt="Anime" />
+            <h2>{name}</h2>
+            <p>{email}</p>
         </div>
     );
 }
