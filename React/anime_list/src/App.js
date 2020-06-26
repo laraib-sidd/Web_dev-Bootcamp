2820629a7e3e96f2958e63da7d33395581278677
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-26 17:26:54 +0530

    update

--- React/anime_list/src/App.js
+++ React/anime_list/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import CardList from './CardList'
 import { robots } from './robots';
+import  SearchBox from './SearchBox'
 
-const App = (robots) => {
+const App = () => {
     return(
+        <div>
+        <h1>My RobotFriends</h1>
+        <SearchBox />
         <CardList robots = {robots}/>
+        </div>
     )
 }
 
---
+++ React/anime_list/src/SearchBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return(
+        <input type="Search" placeholder="search Robots"></input>
+    );
+}
+
+export default SearchBox;
